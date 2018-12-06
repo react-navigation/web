1f9ab0b7c6452e88642482c22076981be0cacc5a
Zobeir Hamid <hamid.zobeir@gmail.com>
2018-12-06 19:25:24 +0900

    allows style prop on Link component

--- src/Link.js
+++ src/Link.js
@@ -20,6 +20,7 @@ class LinkWithNavigation extends Component {
       routeKey,
       navigation,
       action,
+      style,
     } = this.props;
     const topNavigation = getTopNavigation(navigation);
     const topRouter = topNavigation.router;
@@ -62,6 +63,7 @@ class LinkWithNavigation extends Component {
           navigation.dispatch(navAction);
           e.preventDefault();
         }}
+        style={style}
       >
         {children}
       </a>
