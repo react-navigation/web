d96b7a341b5a0763d32d13c8bb310bf7bfd592ea
Derk <dpnolte@gmail.com>
2019-04-28 10:57:41 +0200

    added anchorProps to Link

--- src/Link.js
+++ src/Link.js
@@ -20,6 +20,7 @@ class LinkWithNavigation extends Component {
       routeKey,
       navigation,
       action,
+      anchorProps,
     } = this.props;
     const topNavigation = getTopNavigation(navigation);
     const topRouter = topNavigation.router;
@@ -62,6 +63,7 @@ class LinkWithNavigation extends Component {
           navigation.dispatch(navAction);
           e.preventDefault();
         }}
+        {...anchorProps}
       >
         {children}
       </a>
